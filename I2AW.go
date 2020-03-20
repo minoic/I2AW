c88d8df01085462eaec609ea53377f66e6435e1d
D0000 <cytusd@outlook.com>
2020-03-20 16:03:01 +0800

    fix session expire time to 10 years go mod vendor

--- I2AW.go
+++ I2AW.go
@@ -6,6 +6,12 @@ import (
 )
 
 func main() {
+	/*	beego.BConfig.WebConfig.Session.SessionGCMaxLifetime = 31536000
+		beego.BConfig.WebConfig.Session.SessionCookieLifeTime = 31536000
+		beego.BConfig.WebConfig.Session.SessionName = "5gEmGeJJFuYh7E"
+		beego.BConfig.WebConfig.Session.SessionProvider = "file"
+		beego.BConfig.WebConfig.Session.SessionProviderConfig = "./tmp"
+		beego.BConfig.WebConfig.Session.SessionOn = true*/
 	beego.Router("/", &controllers.IndexController{})
 	beego.Router("/rgbvalue/:identifier", &controllers.RgbValueController{})
 	beego.Run()
---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/MinoIC/I2AW
+
+go 1.13
+
+require (
+	github.com/astaxie/beego v1.12.1
+	github.com/aybabtme/rgbterm v0.0.0-20170906152045-cc83f3b3ce59
+	github.com/jinzhu/gorm v1.9.12
+	github.com/mattn/go-isatty v0.0.12
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
+	github.com/qeesung/image2ascii v1.0.1
+	github.com/shiena/ansicolor v0.0.0-20151119151921-a422bbe96644 // indirect
+	github.com/stretchr/testify v1.5.1
+	github.com/wayneashleyberry/terminal-dimensions v1.0.0
+)
